6da0da851e517e13d5cf6fcf9d386a090bcb3c34
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-12-06 23:22:06 +0900

    docs: add tip contents

--- src/utils/tips.js
+++ src/utils/tips.js
@@ -1,10 +1,19 @@
 export const tips = [
-  ['시뮬레이션을 통해 수익금, 수익률, 배당금 등을 볼 수 있습니다.', 'eng'],
-  ['배당금은 재투자됩니다.', 'eng'],
   [
-    '코스피, 코스닥, 뉴욕, 나스닥, 아멕스 시장의 주식들이 등록되어 있습니다.',
-    'eng',
+    '시뮬레이션을 통해 수익, 배당, 분할 정보를 볼 수 있습니다.',
+    'You can see the earnings, dividends, and split information through the simulation.',
   ],
-  ['환율의 변동이 고려됩니다.', 'eng'],
-  ['달러, 원화로도 시뮬레이션할 수 있습니다', 'eng'],
+  ['배당금은 재투자됩니다.', 'The dividend is reinvested.'],
+  [
+    '코스피, 코스닥, 뉴욕, 나스닥, 아멕스 시장의 주식들을 이용할 수 있습니다.',
+    'Stocks in the KOSPI, KOSDAQ, New York, Nasdaq, and Amex markets are available.',
+  ],
+  [
+    '환율의 변동을 고려하여 시뮬레이션합니다.',
+    'Simulate considering changes in exchange rates.',
+  ],
+  [
+    '달러와 원화로 시뮬레이션할 수 있습니다',
+    'It can be simulated in dollars and won',
+  ],
 ];
